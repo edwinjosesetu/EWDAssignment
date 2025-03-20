528c7025c77fb48c2b02468d4e46545b89643652
edwinjosesetu <20110463@mail.wit.ie>
2025-03-20 19:39:50 +0000

    Add authorizer file

--- shared/types.d.ts
+++ shared/types.d.ts
@@ -7,3 +7,19 @@ export type MovieReviews = {
   reviewDate: string;
   content: string;
 }
+
+export type SignUpBody = {
+  username: string;
+  password: string;
+  email: string
+}
+
+export type ConfirmSignUpBody = {
+  username: string;
+  code: string;
+}
+
+export type SignInBody = {
+  username: string;
+  password: string;
+}
